1800c49f08ec208602dabe89a7700918a27619a5
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2024-06-14 11:54:33 -0300

    add

---
+++ to-do-list/src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-avatar class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/images/boy.png"
+      style="width: 150px; height: 150px"
+    />
+  </q-avatar>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
